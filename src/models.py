7780a471ac100dbb4e03134c5858806e2bd2cb59
Alejandro García López <alejandrogarlo95@gmail.com>
2026-06-08 17:37:42 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -1,61 +1,50 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
 
-class User(Base):
+class User(db.Model):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    email = Column(String(120), unique=True, nullable=False)
-    password = Column(String(80), nullable=False)
-    subscription_date = Column(String(50))
-    first_name = Column(String(50))
-    last_name = Column(String(50))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    subscription_date = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
 
-    favorites = relationship("Favorite", back_populates="user")
+    favorites = db.relationship("Favorite", back_populates="user")
 
 
-class Character(Base):
+class Character(db.Model):
     __tablename__ = 'character'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    gender = Column(String(50))
-    eye_color = Column(String(50))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(50))
+    eye_color = db.Column(db.String(50))
 
-    favorites = relationship("Favorite", back_populates="character")
+    favorites = db.relationship("Favorite", back_populates="character")
 
 
-class Planet(Base):
+class Planet(db.Model):
     __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    population = Column(String(50))
-    climate = Column(String(50))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.String(50))
+    climate = db.Column(db.String(50))
 
-    favorites = relationship("Favorite", back_populates="planet")
+    favorites = db.relationship("Favorite", back_populates="planet")
 
 
-class Favorite(Base):
+class Favorite(db.Model):
     __tablename__ = 'favorite'
-    id = Column(Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(
+        db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    character_id = db.Column(
+        db.Integer, db.ForeignKey('character.id'), nullable=True)
 
-    user = relationship("User", back_populates="favorites")
-    planet = relationship("Planet", back_populates="favorites")
-    character = relationship("Character", back_populates="favorites")
-
-
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+    user = db.relationship("User", back_populates="favorites")
+    planet = db.relationship("Planet", back_populates="favorites")
+    character = db.relationship("Character", back_populates="favorites")
